a1dcb0347525896d09864a1146641e74dc02cbf1
Paradox <Paradox@users.noreply.github.com>
2015-11-08 00:32:55 +0800

    add class standard for matches, modified d2l correspondingly

--- crawler/d2l.py
+++ crawler/d2l.py
@@ -7,6 +7,8 @@ from lxml import etree
 from bs4 import BeautifulSoup as soup
 
 from utils import headers
+from utils import Match
+from utils import tbd
 
 def convert_time(t):
     s = t.split()
@@ -25,32 +27,65 @@ def convert_time(t):
         live = 1
     else:
         live = -1
-    x = absolute * multiplier * sign
-    if x > 0:
-        return '+' + str(x)
-    if x < 0:
-        return str(x)
+    return absolute * multiplier * sign
+
+def crawl_details(webpage, series, notes):
+    print('fetching ' + webpage)
+    time.sleep(1)
+    timestamp = time.time()
+    response = requests.get(webpage, headers=headers)
+    s = soup(response.text, 'lxml')
+    matchtime_rlt = convert_time(s.find('div', {'class': 'half', 'style': 'font-size: 0.8em;width: 33%;'}).text)
+    matchtime = matchtime_rlt + timestamp
+    bestof = s.find('div', {'class': 'half', 'style': 'font-size: 0.8em;text-align: center;width: 28%;'}).text[-1]
+    matchtime_abs = s.find('div', {'class': 'half', 'style': 'font-size: 0.8em;text-align: right;width: 33%;'}).text
+    winner = None
+    teamA = s.find('span', {'style': 'width: 45%; float: left; text-align: right'}).find('b').text
+    if '(win)' in teamA:
+        teamA = teamA[:-6]
+        winner = teamA
+    oddA = float(s.find('span', {'style': 'width: 45%; float: left; text-align: right'}).find('i').text[:-1]) / 100
+    teamB = s.find('span', {'style': 'width: 45%; float: left; text-align: left'}).find('b').text
+    if '(win)' in teamB:
+        teamB = teamB[:-6]
+        winner = teamB
+    oddB = float(s.find('span', {'style': 'width: 45%; float: left; text-align: left'}).find('i').text[:-1]) / 100
+    returnA = 1 + float(s.find('div', {'style': 'float: left; margin: 0.25em 2%;'}).text[5:-6])
+    returnB = 1 + float(s.find('div', {'style': 'float: right; margin: 0.25em 2%;'}).text[5:-6])
+    return Match(
+        active=matchtime_rlt > 0,
+        matchtime=time.strftime('%Y-%m-%d %H:%M', time.localtime(matchtime_rlt + timestamp)),
+        webpage=webpage,
+        serie=series,
+        teams=(teamA, teamB), 
+        odds=(oddA, oddB),
+        returns=(returnA, returnB),
+        notes=notes,
+        winner=winner,
+        )
+
+#s = crawl_details('http://dota2lounge.com/match?m=9794', 'summit', None)
+#print(s)
 
 url = 'http://dota2lounge.com/'
 response = requests.get(url, headers=headers)
 matches = soup(response.text, 'lxml').findAll('div', {'class': 'matchmain'})
 with open('matches_d2l', 'w') as fw:
     for match in matches:
-        href = match.find('a').get('href')
-        print('fetching ' + url + href)
-        time.sleep(1)
-        response = requests.get(url + href, headers=headers)
-        half_tag = soup(response.text, 'lxml').findAll('div', {'class': 'half'})
-        potential = []
-        for value in half_tag:
-            if 'for' in value.text:
-                potential.append(value.text.strip()[5:8])
+        href = url + match.find('a').get('href')
+        #potential = []
+        #for value in half_tag:
+        #    if 'for' in value.text:
+        #        potential.append(value.text.strip()[5:8])
         matchinfo = re.split(r'[\nÂ\xa0]+', match.text.strip())
 
-        note = 'No-note'
+        notes = None
         if len(matchinfo) == 5:
-            t, event, team1, _, team2 = matchinfo
+            t, series, teamA, _, teamB = matchinfo
         if len(matchinfo) == 6:
-            t, note, event, team1, _, team2 = matchinfo
-        fw.write('{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}\n'.format(convert_time(t), event, team1[:-3], team1[-3:], potential[0], team2[:-3], team2[-3:], potential[1], note))
-        fw.flush()
+            t, notes, series, teamA, _, teamB = matchinfo
+        s = crawl_details(href, series, notes)
+        fw.write(str(s))
+        print(str(s))
+        #fw.write('{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}\n'.format(convert_time(t), event, team1[:-3], team1[-3:], potential[0], team2[:-3], team2[-3:], potential[1], note))
+        #fw.flush()
--- crawler/utils.py
+++ crawler/utils.py
@@ -2,16 +2,51 @@
 
 import os
 import time
+import json
+from slugify import slugify
 
 headers = {
 'User-Agent':'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.93 Safari/537.36',
 }
+tbd = 'TBD'
+
+class Match(object):
+    def __init__(self, active='', matchtime='1990-01-01 00:00', webpage='', serie='', teams=(tbd, tbd), odds=(0, 0), returns=(0, 0), notes=None, winner=''):
+        self.active = active
+        self.matchtime = matchtime
+        self.serie = slugify(serie)
+        teams = [slugify(team) for team in teams]
+        if teams[0] <= teams[1]:
+            self.teams = teams
+            self.odds = odds
+            self.returns = returns
+        else:
+            self.teams = teams[::-1]
+            self.odds = odds[::-1]
+            self.returns = returns[::-1]
+        self.notes = notes
+        self.webpage = webpage
+
+    def indentical(self, s):
+        assert(isinstance(s, match))
+        if abs(time.mktime(self.matchtime) - time.mktime(s.matchtime)) <= 5400 and self.serie == s.serie and self.team == s.team:
+            return True
+        else:
+            return False
+
+    def __str__(self):
+        return json.dumps(self.__dict__)
+
+    @staticmethod
+    def load(s):
+        obj = match(active='', matchtime='', webpage='')
+        obj.__dict__ = json.loads(s)
+        return obj
 
 def ww(d, f, m='w'):
     t = time.localtime()
     s = time.strftime('%Y-%m-%d', t)
     return open(os.path.join(d, '{0}_{1}'.format(f, s)), m)
-    
     
 def rr(d, f):
     l = os.listdir(d)
