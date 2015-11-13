fcb4ad364458757f638f7a475e290c259c2b9562
MRain <linmrain@gmail.com>
2015-11-13 21:05:33 +0800

    best_of & bets in nxt

--- crawler/nxt.py
+++ crawler/nxt.py
@@ -18,16 +18,18 @@ def crawl_match(match_id):
     content = soup(response.content, 'html.parser')
     league = content.find('div', {'class': 'col-xs-6 text-left'}).p.text
     isLive = False
+    isCompleted = False
     if len(content.findAll('span', {'class': 'text-right small tickers_match_details_live'})) > 0:
         isLive = True
         ticker = content.find('span', {'class': 'text-right small tickers_match_details_live'})
         match_time = datetime.datetime.strptime(ticker.get('data-date-time'), '%Y,%m,%d,%H,%M,%S')
     elif len(content.findAll('p', {'class': 'text-right small tickers_match_details'})) > 0:
         ticker = content.find('p', {'class': 'text-right small tickers_match_details'})
-        #match_time = ticker.get('data-date-time')
         match_time = datetime.datetime.strptime(ticker.get('data-date-time'), '%Y,%m,%d,%H,%M,%S')
-        #print('Match time: ' + match_time)
-        #match['time'] = match_time
+    elif len(content.findAll('p', {'class': 'text-right small tickers_match_completed'})) > 0:
+        isCompleted = True
+        ticker = content.find('p', {'class': 'text-right small tickers_match_completed'})
+        match_time = datetime.datetime.strptime(ticker.get('data-date-time'), '%Y,%m,%d,%H,%M,%S')
     # Teams
     teamA = content.find('div', {'class': 'col-xs-6 text-center col-xs-height col-top teamA'})
     teamA_tag = teamA.find('div', {'class': 'col-xs-6 text-center'})
@@ -35,24 +37,20 @@ def crawl_match(match_id):
     teamA_rate = teamA_tag.p.next_sibling.next_sibling.text.strip()
     teamA_ID = teamA.find('input', {'class': 'teamID'}).get('value')
     teamA_rewards = content.find('div', {'class': 'col-xs-6 col-md-3 text-center odds-panel-teamA'}).span.text
-    #print('TeamA: ' + teamA_name + ' (' + teamA_rate + '), rewards ' + teamA_rewards)
-    #match['team'] = [teamA_name]
-    #match['odds'] = [teamA_rate]
-    #match['rewards'] = [teamA_rewards]
+    
     teamB = content.find('div', {'class': 'col-xs-6 text-center col-xs-height col-top teamB'})
     teamB_tag = teamB.find('div', {'class': 'col-xs-6 text-center'})
     teamB_name = teamB_tag.p.text.strip()
     teamB_rate = teamB_tag.p.next_sibling.next_sibling.text.strip()
     teamB_ID = teamB.find('input', {'class': 'teamID'}).get('value')
     teamB_rewards = content.find('div', {'class': 'col-xs-6 col-md-3 text-center odds-panel-teamB'}).span.text
-    #print('TeamB: ' + teamB_name + ' (' + teamB_rate + '), rewards ' + teamB_rewards)
-    #match['team'].append(teamB_name)
-    #match['odds'].append(teamB_rate)
-    #match['rewards'].append(teamB_rewards)
-    bets = content.find('h5').text.strip()
-    #print(bets)
-    #match['bets'] = bets
-    #return match
+
+    try:
+        bets = re.match(r"\d+", content.find('h5').text.strip()).group()
+        best_of = re.search(r"Best of (?P<haha>\d)", content.text).group('haha')
+    except:
+        bets = -1
+        best_of = -1
     return Match(
         active = str(isLive),
         matchtime=str(match_time),
@@ -61,7 +59,8 @@ def crawl_match(match_id):
         teams=(teamA_name, teamB_name),
         odds=(teamA_rate, teamB_rate),
         returns=(teamA_rewards, teamB_rewards),
-        notes='Number of bets: ' + str(bets)
+        poolsize=bets,
+        bestof=best_of
         )
 
 def crawl_match_list():
@@ -76,7 +75,7 @@ def crawl_match_list():
 
 def flowtest():
     print('nxt crawler flowtest')
-    print('crawling http://dota2lounge.com/')
+    print('crawling http://nxtgame.com/')
     print('Result:')
     match_list = crawl_match_list()
     for match_id in match_list:
