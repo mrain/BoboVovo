3600780ec79c48346bab2c4953a2f4a66dad2f39
Paradox <Paradox@users.noreply.github.com>
2015-11-21 23:01:04 +0800

    readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# BoboVovo
+
+Stranger, welcome to the best ~~bontting~~research project on earth.
+
+## Dependency
+
+* python
+* python-slugify from pip
+* a wooden desktop with a plastic keyboard
+   
+## Key Components
+
+* notificer
+* collector
+* pesticide to save our wooden desktop
--- collect.py
+++ collect.py
@@ -1,10 +1,13 @@
 from itertools import chain
+import json
+
 from crawler import __all__ as all_crawlers
 from crawler import *
 all_crawlers = [globals()[crawler] for crawler in all_crawlers]
 
-inactive = json.load('inactive.log')
+inactive = json.load(open('inactive.log'))
 
 with open('match.log', 'a') as fw:
     for match in chain(*[crawler.crawl_full() for crawler in all_crawlers]):
-        fw.write(match.__str__ + '\n')
+        print(match)
+        fw.write(match.__str__() + '\n')
